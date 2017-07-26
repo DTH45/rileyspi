7cdec629b967c0594b2c3bb2b8f07969b9aace6b
DTH45 <rileyryff@gmail.com>
2017-07-26 15:02:20 -0700

    Update

--- index.php
+++ index.php
@@ -35,7 +35,7 @@
 		</div>
 
 		<div id="container">
-			<div id="header">=Controls=</div>
+			<div id="header">Server Controls:</div>
 			<?php
 				if (isset($_POST['reboot']))
 					{
@@ -66,5 +66,84 @@
 				<button class="btn" name="voldwn">Volume Down</button>
 			</form>
 		</div>
+
+		<div id="container">
+			<div id="header">GPIO Controls:</div>
+			<?php
+				if (isset($_POST['1on']))
+					{
+						exec("./home/pi/pythonweb/relay1on.py");
+					}
+				if (isset($_POST['1off']))
+					{
+						exec("./home/pi/pythonweb/relay1off.py");
+					}
+				if (isset($_POST['2on']))
+					{
+						exec("./home/pi/pythonweb/relay2on.py");
+					}
+				if (isset($_POST['2off']))
+					{
+						exec("./home/pi/pythonweb/relay2off.py");
+					}
+				if (isset($_POST['3on']))
+					{
+						exec("./home/pi/pythonweb/relay3on.py");
+					}
+				if (isset($_POST['3off']))
+					{
+						exec("./home/pi/pythonweb/relay3off.py");
+					}
+				if (isset($_POST['4on']))
+					{
+						exec("./home/pi/pythonweb/relay4on.py");
+					}
+				if (isset($_POST['4off']))
+					{
+						exec("./home/pi/pythonweb/relay4off.py");
+					}
+				if (isset($_POST['5on']))
+					{
+						exec("./home/pi/pythonweb/relay5on.py");
+					}
+				if (isset($_POST['5off']))
+					{
+						exec("./home/pi/pythonweb/relay5off.py");
+					}
+				if (isset($_POST['6on']))
+					{
+						exec("./home/pi/pythonweb/relay6on.py");
+					}
+				if (isset($_POST['6off']))
+					{
+						exec("./home/pi/pythonweb/relay6off.py");
+					}
+				if (isset($_POST['reset']))
+					{
+						exec("./resetpins.py");
+					}
+			?>
+			<form method="post">
+				<div id="relaycont">
+					<div id="subhead">Relay 1:</div>
+					<button class="btn" name="1on">On</button>
+					<button class="btn" name="1off">Off</button>
+				</div>
+
+				<div id="relaycont">
+					<div id="subhead">Relay 2:</div>
+					<button class="btn" name="2on">On</button>
+					<button class="btn" name="2off">Off</button>
+				</div>
+
+				<div id="relaycont">
+					<div id="subhead">Relay 3:</div>
+					<button class="btn" name="3on">On</button>
+					<button class="btn" name="3off">Off</button>
+				</div>
+			</form>
+		</div>
+
+
 	</body>
 </html>
